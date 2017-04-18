b5c71616dae47b1edd6fa466995c4fbcf5f7f4dc
Jeremy Liberman <jliberman@strenuus.com>
2017-04-18 12:28:38 -0500

    Use HashRouter in demo site

--- example/src/index.js
+++ example/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './components/App'
 import './index.css'
 
 import store from './lib/store'
 
 ReactDOM.render(
-  <BrowserRouter><Provider store={store}><App /></Provider></BrowserRouter>,
+  <HashRouter><Provider store={store}><App /></Provider></HashRouter>,
   document.getElementById('root')
 )
